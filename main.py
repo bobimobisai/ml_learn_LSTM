9ac94e5cfac4179691b644a472e8756634d38a78
ivan <danilgrachev1992@gmail.com>
2024-09-14 15:40:58 +0300

    upd ports

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 from load_model import start_update_thread
 import queue
-from test import get_qeustion, ptint_response
-import threading
+import uvicorn
+import logging
 
+
+logging.basicConfig(level=logging.INFO)
 
 _queue = queue.Queue()
 output_queue = queue.Queue()
 
+app = FastAPI()
+
+start_update_thread(_queue, output_queue)
+
+
+class QuestionRequest(BaseModel):
+    question: str
+
+
+@app.post("/ask/")
+async def ask_question(request: QuestionRequest):
+    try:
+        _queue.put(request.question)
+        response, confidence = output_queue.get()
+        confidence = float(confidence)
+        if confidence > 0.5:
+            return JSONResponse(
+                content={"response": response, "confidence": confidence},
+                status_code=201,
+            )
+        else:
+            return JSONResponse(
+                content={"response": "Не уверен...", "confidence": confidence},
+                status_code=202,
+            )
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
 
 if __name__ == "__main__":
-    start_update_thread(_queue, output_queue)
-    th1 = threading.Thread(target=get_qeustion, args=(_queue,))
-    th2 = threading.Thread(target=ptint_response, args=(output_queue,))
-
-    th1.start()
-    th2.start()
+    uvicorn.run(app, host="0.0.0.0", port=9092)
--- test.py
+++ test.py
@@ -1,11 +1,22 @@
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
+
+
 def get_qeustion(queue):
     while True:
         input_text = input("Ваш вопрос: ")
         queue.put(input_text)
 
+
 def ptint_response(output_queue):
     while True:
         response = output_queue.get()
         if response:
-            print(f"Ответ: {response[0]}")
-            print(f"Уверенность: {response[1]}")
+            if response[1] > 0.5:
+                logging.info(f"Ответ: {response[0]}")
+                logging.info(f"Уверенность: {response[1]}")
+            else:
+                logging.info(f"Ответ: Не уверен...")
+                logging.info(f"Уверенность: {response[1]}")
